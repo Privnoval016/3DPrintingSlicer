e7d0c58dd961fc35a07bbaed7efa6d69d6f50cc2
Renovic <mightymickey81@gmail.com>
2025-09-20 21:46:37 -0400

    Fixed Perimeter Generator

--- 3DPrintingSlicer/Perimeters/PerimeterGenerator.py
+++ 3DPrintingSlicer/Perimeters/PerimeterGenerator.py
@@ -1,38 +1,62 @@
 from shapely import Polygon
 import numpy as np
 from collections import defaultdict
-
+import matplotlib.pyplot as plt
+from shapely import plotting
 import shapely
-
 from LayerSlicing.ZSlice import ZSlice
 
 class PerimeterGenerator:
     def create_polygons(self, z_slice):
+    
         adjacency = defaultdict(list)
-        for edge in z_slice.edges:
-            adjacency[edge[0]].append(edge[1])
-            adjacency[edge[1]].append(edge[0])
-        
-        polygon_indices = []    
-        current_polygon = []
+        for a, b in z_slice.edges:
+            adjacency[a].append(b)
+            adjacency[b].append(a)
 
-        edge = adjacency[0]
-        while len(adjacency) > 0:
-            next = 0
-            if adjacency.get(edge[0]) is not None:
-                next = edge[0]
-            elif adjacency.get(edge[1]) is not None:
-                next = edge[1]
-            else :
-                next = list(adjacency.keys())[0]
-                polygon_indices.append(current_polygon)
-            current_polygon.append(edge[1])
-            edge = adjacency[next]
-            del adjacency[next]
+        polygon_indices = []
+        visited_edges = set()
 
-        if len(current_polygon) > 0:
-            polygon_indices.append(current_polygon)
-        
+        while adjacency:
+            # Start from any remaining vertex
+            start = next(iter(adjacency))
+            current = start
+            prev = None
+            polygon = [start]
+
+            while True:
+                neighbors = adjacency[current]
+                # pick the next neighbor
+                if prev is None:
+                    nxt = neighbors[0]
+                else:
+                    nxt = neighbors[0] if neighbors[0] != prev else neighbors[1]
+
+                edge = tuple(sorted((current, nxt)))
+                if edge in visited_edges:
+                    break
+                visited_edges.add(edge)
+
+                polygon.append(nxt)
+
+                # advance
+                prev, current = current, nxt
+
+                if current == start:
+                    break
+
+            polygon_indices.append(polygon)
+
+            # cleanup: remove used vertices (no remaining neighbors)
+            for a, b in zip(polygon, polygon[1:]):
+                adjacency[a].remove(b)
+                adjacency[b].remove(a)
+                if not adjacency[a]:
+                    del adjacency[a]
+                if not adjacency[b]:
+                    del adjacency[b]
+
+            
         all_polygons = []
         for vertex_indices in polygon_indices:
             vertices = []
@@ -69,5 +93,5 @@ class PerimeterGenerator:
         perimeters = []
         for polygon in self.polygons:
             for i in range(wall_count):
-                perimeters.append(shapely.buffer(polygon, (-1) * line_width * i))
+                perimeters.append(shapely.buffer(polygon, (-1) * line_width * (i+1)))
         return perimeters
--- 3DPrintingSlicer/main.py
+++ 3DPrintingSlicer/main.py
@@ -3,18 +3,17 @@ from Rendering.LayerRenderer import LayerRenderer
 from Perimeters.PerimeterGenerator import PerimeterGenerator
 
 def main():
-    file_name = "STLFiles/20mm_cube.stl"
+    file_name = "STLFiles/LowPolyBenchy.stl"
 
     z_slicer = ZSlicer()
-    z_slicer.compute_slices_from_stl(True, file_name, specify_height=False, num=100)
+    z_slicer.compute_slices_from_stl(False, file_name, specify_height=False, num=25)
 
     #layer_renderer = LayerRenderer(z_slicer)
     #layer_renderer.render_all_slices()
 
     for z_slice in z_slicer.get_slices():
         perimeter_generator = PerimeterGenerator(z_slice)
-        perimeter_generator.createPerimeters(0.5,2)
-
+        perimeter_generator.createPerimeters(2,2)
 
 
 if __name__ == "__main__":
