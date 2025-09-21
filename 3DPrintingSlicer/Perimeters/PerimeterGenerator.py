a57697ba572827d72851d29548f825cd9639785e
Pranav Sukesh <pranavsukesh@gmail.com>
2025-09-20 21:53:58 -0400

    erm what the sigma

--- 3DPrintingSlicer/Perimeters/PerimeterGenerator.py
+++ 3DPrintingSlicer/Perimeters/PerimeterGenerator.py
@@ -6,9 +6,10 @@ from shapely import plotting
 import shapely
 from LayerSlicing.ZSlice import ZSlice
 
+
 class PerimeterGenerator:
     def create_polygons(self, z_slice):
-    
+
         adjacency = defaultdict(list)
         for a, b in z_slice.edges:
             adjacency[a].append(b)
@@ -56,42 +57,44 @@ class PerimeterGenerator:
                 if not adjacency[b]:
                     del adjacency[b]
 
-            
         all_polygons = []
         for vertex_indices in polygon_indices:
             vertices = []
             for vertex in vertex_indices:
                 vertices.append(z_slice.vertices[vertex])
             all_polygons.append(Polygon(vertices))
-            
+
         depth = []
         for polygon in all_polygons:
             point = polygon.representative_point()
-            depth.append(sum(1 for other in all_polygons if other != polygon and other.contains(point)))
-        
+            depth.append(sum(1 for other in all_polygons if
+                             other != polygon and other.contains(point)))
+
         polygons = []
         polygon_depths = []
         for i, polygon in enumerate(all_polygons):
-            if depth[i] % 2 == 0 :
+            if depth[i] % 2 == 0:
                 polygon_depths.append(depth[i])
                 polygons.append(polygon)
-        
+
         for i, polygon in enumerate(all_polygons):
-            if depth[i] % 2 == 1 :
+            if depth[i] % 2 == 1:
                 for j, other in enumerate(polygons):
-                    if depth[i] + 1 == polygon_depths[j] and other.contains(polygon.reprsentative_point()):
-                        other = shapely.union_all((other,polygon))
-        
+                    if depth[i] + 1 == polygon_depths[j] and other.contains(
+                            polygon.reprsentative_point()):
+                        other = shapely.union_all((other, polygon))
+
         return polygons
-    
+
     def __init__(self, z_slice):
         self.z_slice = z_slice
-        self.polygons = self.create_polygons(z_slice) # Set of polygons: Outer Contour, and hole with Inner Contour
+        self.polygons = self.create_polygons(
+            z_slice)  # Set of polygons: Outer Contour, and hole with Inner Contour
 
-        
     def createPerimeters(self, line_width, wall_count):
         perimeters = []
         for polygon in self.polygons:
             for i in range(wall_count):
-                perimeters.append(shapely.buffer(polygon, (-1) * line_width * (i+1)))
+                perimeters.append(
+                    shapely.buffer(polygon, (-1) * line_width * (i + 1)))
         return perimeters
--- 3DPrintingSlicer/main.py
+++ 3DPrintingSlicer/main.py
@@ -6,23 +6,14 @@ def main():
     file_name = "STLFiles/LowPolyBenchy.stl"
 
     z_slicer = ZSlicer()
-<<<<<<< Updated upstream
-    z_slicer.compute_slices_from_stl(False, file_name, specify_height=False, num=25)
-=======
-    z_slicer.compute_slices_from_stl(file_name, specify_height=False, num=100)
->>>>>>> Stashed changes
+    z_slicer.compute_slices_from_stl(file_name, specify_height=False, num=25)
 
     #layer_renderer = LayerRenderer(z_slicer)
     #layer_renderer.render_all_slices()
 
     for z_slice in z_slicer.get_slices():
         perimeter_generator = PerimeterGenerator(z_slice)
-<<<<<<< Updated upstream
         perimeter_generator.createPerimeters(2,2)
-=======
-        something = perimeter_generator.createPerimeters(0.5,2)
-
->>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
